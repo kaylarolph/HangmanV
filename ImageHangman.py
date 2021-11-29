edeb2996934a18e6e6f0bd04e2fcef50c2750cdd
ellieyoon <eyoon@potomacschool.org>
2021-11-29 09:57:54 -0500

    Images

--- ImageHangman.py
+++ ImageHangman.py
@@ -2,8 +2,9 @@ import pygame
 import math
 import sys
 import random
+
 done = False
-while done == False:
+while done == False: #while the user has not won the game, the code keeps running/will restart and let user play again
     #Initial Settings and baseline Variables
     pygame.init()
 
@@ -23,26 +24,21 @@ while done == False:
     BLACK = (0, 0 , 0)
 
     #Buttons for letters A-Z
-    RADIUS = 20
-    GAP = 15
+    RADIUS = 20 #move this to more releveant location
+
     letters = [] #List that stores created buttons [x, y, chr(A+i), True]
-    startx = round(WIDTH - ((RADIUS * 2 + GAP) * 13)) #Width of screen - (diameter of 13 buttons w/ gap's in between) = starting position
-    starty = 400
     A = 65 #A = 65, B = 66, C = 67,  chr(A+i) - character that corresponds to 65, 66, 67...
 
     for i in range(26): #for the 26 letters of the alphabet, store where it should be positioned
-        x = startx + GAP * 2 + ((RADIUS * 2 + GAP)* (i % 13))
-        # each loop, i tells us what button we're on,
-        #GAP * 2 so you are not directly on sides of screen, simulate having two rows with i%13, (RADIUS* 2 + GAP) = distance between each button
-        y = starty + ((i // 13) * (GAP + RADIUS * 2)) #integer division with i//13
+        x = 40 + 55 *(i % 13) #x position of each letter button - 2 rows of 13
+        y = 400 + 55 * (i//13) #y position of each letter button
         letters.append([x,y,chr(A + i), True]) #pairs of x, y values into the list, store 4 things in each letter
 
     #Loading the hangman images from the uploaded folder
-    images = []
-    for i in range(9): #loop 0,1,2,3,4,5,6,7,8,9
+    folderimages = []
+    for i in range(9): #loops from 0 to 8, adding the 9 hangman images (ordered numerically) from the folder
         image = pygame.image.load("Hangman_Images!/Hangman_Images! (" +  str(1+i) + ").png")
-        #image = pygame.image.load("HangmanImages/IMG_" + str(7521 + i) + ".jpg") #first image is IMG_7521.jpg, second is 7522..
-        images.append(image)
+        folderimages.append(image)
 
     hangman_status = 0 #put this where it is more obviously relevant
     guessed = [] #list that keeps track of letters guessed so far
@@ -53,28 +49,14 @@ while done == False:
              "PROVEN", "HEALTHY", "TEACHER", "STUDENT", "HOT", "COLD", "TISSUE", "SWEATER",
               "PUSH", "HAIR", "BLONDE", "BLOCK", "WORK", "MASK", "ROBOT", "TOUCH", "HAT", "ENTER", "TABLE", "CHAIR"
             ]
-
-
-    #Functions or methods - what are these called
     def selectRandomWord(wordList): #this function selects a random string from the the list of available strings
         indexOfWord = random.randint(0, len(wordList) - 1)
         return wordList[indexOfWord]
 
-   # def draw(): #this function draws the guessed letters and buttons onto the screen
 
+#The actual code begins here
 
-    def display_message(message): #This function overrides the screen and tells the user if they won or lost
-        pygame.time.delay(1000) #wait one sec. bf drawing anything
-        window.fill(WHITE)  # override everything on the screen
-        text = WORD_FONT.render(message, True, BLACK)
-        window.blit(text,(250, 200))
-        pygame.display.update()
-        pygame.time.delay(4000) #3 seconds
-
-
-
-    #The actual code begins here
-
+    #Code for the welcome screen
     keynotpressed = True #key has not been pressed
     while keynotpressed:
         window.fill(WHITE)
@@ -94,6 +76,7 @@ while done == False:
                 keynotpressed = False
                 break
 
+    #Code for the actual Hangman game
     word = selectRandomWord(wordList)
     run = True
 
@@ -132,26 +115,35 @@ while done == False:
                 text = LETTER_FONT.render(ltr, 1, BLACK)
                 window.blit(text, (x - text.get_width() / 2, y - text.get_height() / 2))
             # you want to draw the letters in the middle of button- go backwards and up
-        window.blit(images[hangman_status], (0, 0))  # blit stands for draw image, surface
+        window.blit(folderimages[hangman_status],(0, 0))
         pygame.display.update()
 
+    #Code displays if the player has won or lost on the screen
         won = True
         for letter in word: #if we loop through every letter in word and all are in guessed, won stays True
             if letter not in guessed:
                 won = False
                 break
+
         if won:
-            display_message("You Won!")
-            done = True
-            break
+             pygame.time.delay(1000)  # wait one sec. bf drawing anything
+             window.fill(WHITE)  # override everything on the screen
+             text = WORD_FONT.render("You Won!", True, BLACK)
+             window.blit(text, (250, 200))
+             pygame.display.update()
+             pygame.time.delay(4000)  #4 seconds
+             done = True
+             break
 
         if hangman_status == 8: #full limbs - you have lost! may need to change this number
-            display_message("You Lost!")
+            pygame.time.delay(1000)  # wait one sec. bf drawing anything
+            window.fill(WHITE)  # override everything on the screen
+            text = WORD_FONT.render("You Lost!", True, BLACK)
+            window.blit(text, (250, 200))
+            pygame.display.update()
+            pygame.time.delay(4000)  # 4 seconds
             done = False
             break
-
+    #this code is part of the while loop which makes the hangman game able to be replayed - or not.
     if done:
         pygame.quit()
-
-#26 total buttons, 2 rows of 13 buttons
-#(Width - ((Gap + radius * 2)*13))/2
